70bfeacd5a9012e443ef01c617e856366b7abd3a
Alexander Rogovskiy <alex@localhost.localdomain>
2022-09-14 14:29:37 +0300

    Use pytest for tests

--- test_compare.py
+++ test_compare.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import pytest
 import random
 import codecs
 import sys
@@ -14,6 +15,7 @@ separator = "|"
 def random_string(length):
     return codecs.encode(os.urandom(length), 'base64').decode().strip()
 
+
 def get_line():
     res = '/'.join( [random_string(random.randint(7, 14)) for _ in range(4)] )
     if not res.startswith('/'):
@@ -21,22 +23,28 @@ def get_line():
     res += separator + str(random.randint(1,10000000)) + separator + random_string(6)
     return res
 
+
 def generate_dumps(tmpdir, base_filename, prefix_num=10, lines=10000, nfiles=3, bad_files=100, spread_across=2):
     base_filename = tmpdir + '/' + base_filename
     orig_filename = base_filename + str(nfiles)
+    line_cnt = lines*prefix_num if prefix_num > 0 else lines
     with open(orig_filename, 'w') as fd:
-        for _ in range(lines*prefix_num):
+        for _ in range(line_cnt):
             print(  get_line(), file=fd  )
 
     res = {}
     for i in range(1, nfiles+1):
-        prefixes = [random_string(random.randint(7, 14)) for i in range(prefix_num)]
+        if prefix_num > 0:
+            prefixes = [random_string(random.randint(7, 14)) for i in range(prefix_num)]
+        else:
+            prefixes = [ '' ]
         filename = base_filename + str(i)
         res[filename] = {'prefixes': prefixes, 'lost': None}
         if i != nfiles:
             copyfile(orig_filename, filename)
-        for pnum in range(prefix_num):
-            call(["/bin/sed", "-i", filename, "-e", f"{pnum*lines+1},{(pnum+1)*lines}s!^!{'/' + prefixes[pnum]}!"])
+        if prefix_num > 0:
+            for pnum in range(prefix_num):
+                call(["/bin/sed", "-i", filename, "-e", f"{pnum*lines+1},{(pnum+1)*lines}s!^!{'/' + prefixes[pnum]}!"])
 
     #Spoil files
     indexes = [i+1 for i in range(nfiles)]
@@ -47,13 +55,13 @@ def generate_dumps(tmpdir, base_filename, prefix_num=10, lines=10000, nfiles=3, 
         filename = base_filename + str(idx)
         removed = []
         for i in range(n_lost):
-            row_num = random.randint(1, lines*nfiles - i)
+            row_num = random.randint(1, line_cnt - i)
             p = Popen(["/bin/sed", "-ne", f"{row_num}p", filename], stdout=PIPE)
             stdout, e_cod = p.communicate()
             call(["/bin/sed", "-i", f"{row_num}d", filename])
             line = stdout.decode().strip()
             for pref in res[filename]['prefixes']:
-                if line.startswith('/' + pref):
+                if line.startswith('/' + pref) and pref:
                     line = line[(len(pref) + 1):]
                 try:
                     line, _ = line.split(separator, maxsplit=1)
@@ -65,16 +73,36 @@ def generate_dumps(tmpdir, base_filename, prefix_num=10, lines=10000, nfiles=3, 
     return res
 
 
-
-if __name__ == '__main__':
-    prefixes = 10
-    n_files = 3
+@pytest.mark.parametrize(
+        "prefixes,n_files,lines",
+        [
+            (0, 3, 10000),
+            (0, 4, 10000),
+            (0, 5, 10000),
+            (3, 3, 10000),
+            (3, 4, 10000),
+            (3, 5, 10000),
+            (6, 3, 10000),
+            (6, 4, 10000),
+            (6, 5, 10000),
+            (9, 3, 10000),
+            (9, 4, 10000),
+            (9, 5, 10000),
+	    (10, 3, 10000),
+            (10, 4, 10000),
+            (10, 5, 10000),
+            (10, 3, 100000),
+            (10, 3, 1000000),
+        ]
+    )
+def test_script(prefixes, n_files, lines):
     tmpdir = './tests'
     base_name = 'gen_dump'
-    dumps = generate_dumps(tmpdir, base_name, prefix_num=prefixes, nfiles=n_files, lines=10000)
+    dumps = generate_dumps(tmpdir, base_name, prefix_num=prefixes, nfiles=n_files, lines=lines)
     missed_files = {k: [] for k in dumps.keys()}
-    for i in range(prefixes):
-        opt = ','.join([f"{dump}%/{dumps[dump]['prefixes'][i]}%|" for dump in dumps])
+    for i in range(prefixes if prefixes != 0 else 1):
+        start_sym = '/' if prefixes > 0 else ''
+        opt = ','.join([f"{dump}%{start_sym}{dumps[dump]['prefixes'][i]}%|" for dump in dumps])
         print(['./compare_v2.py', '-t', './tests', '-d', opt])
         p = Popen(['./compare_v2.py', '-t', './tests', '-d', opt], stdout=PIPE)
         stdout, stderr = p.communicate()
@@ -93,15 +121,10 @@ if __name__ == '__main__':
     for key in dumps:
         if dumps[key]['lost'] is not None:
             try:
-                if set(dumps[key]['lost']) != set(missed_files[key]):
-                    print(f"Mismatch: {key}")
-                    print(f"{sorted(dumps[key]['lost'])}")
-                    print(f"{sorted(missed_files[key])}")
-                    bad = True
-                    sys.exit(1)
-            except Exception as e:
-                print(f"Except {e}", key, dumps, missed_files)
-                sys.exit(1)
+                assert set(dumps[key]['lost']) == set(missed_files[key])
+            except AssertionError:
+                print("Failed:")
 
-    if not bad:
-        print("All OK")
+
+if __name__ == '__main__':
+    test_script(0,3,10000)
