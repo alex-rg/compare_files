c96500cf70d9d8fc486f3d1ee4209bb16924bf7c
Alexander Rogovskiy <alex@localhost.localdomain>
2023-01-22 15:59:15 +0300

    feat: updated tests

--- test_compare.py
+++ test_compare.py
@@ -10,6 +10,7 @@ from shutil import copyfile
 from subprocess import call, Popen, PIPE
 
 separator = "|"
+TMPDIR = './tests'
 
 
 def random_string(length):
@@ -54,23 +55,55 @@ def generate_dumps(tmpdir, base_filename, prefix_num=10, lines=10000, nfiles=3, 
         indexes.remove(idx)
         filename = base_filename + str(idx)
         removed = []
+        row_nums = set()
         for i in range(n_lost):
-            row_num = random.randint(1, line_cnt - i)
-            p = Popen(["/bin/sed", "-ne", f"{row_num}p", filename], stdout=PIPE)
-            stdout, e_cod = p.communicate()
-            call(["/bin/sed", "-i", f"{row_num}d", filename])
-            line = stdout.decode().strip()
-            for pref in res[filename]['prefixes']:
-                if line.startswith('/' + pref) and pref:
-                    line = line[(len(pref) + 1):]
-                try:
-                    line, _ = line.split(separator, maxsplit=1)
-                except:
-                    pass
-            removed.append(line)
+            row_nums.add(random.randint(1, line_cnt - i))
 
-        res[filename]['lost'] = removed
+        row_nums = sorted([x for x in row_nums])
+        if len(row_nums) > 0:
+            row_str = 'p;'.join(str(x) for x in row_nums)
+        else:
+            row_str = None
+
+        if row_str is not None:
+            p = Popen(["/bin/sed", "-ne", f"{row_str}p", filename], stdout=PIPE)
+            stdout, e_cod = p.communicate()
+            for line in stdout.decode().split('\n'):
+                line = line.strip()
+                if line:
+                    for pref in res[filename]['prefixes']:
+                        if line.startswith('/' + pref) and pref:
+                            line = line[(len(pref) + 1):]
+                        try:
+                            line, _ = line.split(separator, maxsplit=1)
+                        except:
+                            pass
+                    removed.append(line)
+            res[filename]['lost'] = removed
+            row_str = row_str.replace('p', 'd')
+            call(["/bin/sed", "-i", f"{row_str}d", filename])
     return res
+
+def test_static():
+    res = 'fsdfadsf\nqwerty\nttttwwwwwww'
+    base_filename = TMPDIR + '/gen_dump'
+    with open(f'{base_filename}1', 'w') as fd:
+        fd.write(res)
+
+    for tres, data in [('aaa', 'aaa\n' + res), ('zzz', res + '\nzzz'), ('', res)]:
+        for i in range(2,4):
+            with open(f'{base_filename}{i}', 'w') as fd:
+                fd.write(data)
+        opt = ','.join(f'{base_filename}{i}%%' for i in range(1,4))
+        print(['./compare_v2.py', '-t', TMPDIR, '-d', opt])
+        p = Popen(['./compare_v2.py', '-t', TMPDIR, '-d', opt], stdout=PIPE)
+        stdout, stderr = p.communicate()
+        if tres != '':
+            data = literal_eval(stdout.decode())
+            assert [x for x in data.keys()] == [tres]
+            assert [x for x in data.values()] == [ [f'{base_filename}1_sorted'] ]
+        else:
+            assert tres == ''
 
 
 @pytest.mark.parametrize(
@@ -88,23 +121,29 @@ def generate_dumps(tmpdir, base_filename, prefix_num=10, lines=10000, nfiles=3, 
             (9, 3, 10000),
             (9, 4, 10000),
             (9, 5, 10000),
-	    (10, 3, 10000),
+            (10, 3, 10000),
             (10, 4, 10000),
             (10, 5, 10000),
             (10, 3, 100000),
-            (10, 3, 1000000),
+            (0, 3, 1000000),
         ]
     )
 def test_script(prefixes, n_files, lines):
-    tmpdir = './tests'
+    tmpdir = TMPDIR
     base_name = 'gen_dump'
     dumps = generate_dumps(tmpdir, base_name, prefix_num=prefixes, nfiles=n_files, lines=lines)
     missed_files = {k: [] for k in dumps.keys()}
+    n_cpus = '3'
+    extra_opts = ['-n', n_cpus]
+    fname_suffix = ''
     for i in range(prefixes if prefixes != 0 else 1):
         start_sym = '/' if prefixes > 0 else ''
-        opt = ','.join([f"{dump}%{start_sym}{dumps[dump]['prefixes'][i]}%|" for dump in dumps])
-        print(['./compare_v2.py', '-t', './tests', '-d', opt])
-        p = Popen(['./compare_v2.py', '-t', './tests', '-d', opt], stdout=PIPE)
+        opt = ','.join([f"{dump}{fname_suffix}%{start_sym}{dumps[dump]['prefixes'][i]}%|" for dump in dumps])
+        print(['./compare_v2.py', '-t', tmpdir, '-d', opt] + extra_opts)
+        p = Popen(['./compare_v2.py', '-t', tmpdir, '-d', opt] + extra_opts, stdout=PIPE)
+        fname_suffix = '_sorted'
+        extra_opts = ['-s']
+
         stdout, stderr = p.communicate()
         for line in stdout.decode().split('\n'):
             try:
