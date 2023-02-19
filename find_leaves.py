a3ee8a4b24d9b556aaa675916d7645d9989ba6dd
Alexander Rogovskiy <arogovsk@lxplus706.cern.ch>
2023-02-19 17:57:16 +0100

    feat: make script able to create dfc dumps

--- find_leaves.py
+++ find_leaves.py
@@ -11,6 +11,17 @@ from DIRAC.Core.Base import Script
 from DIRAC.Core.Utilities.List import breakListIntoChunks
 from DIRAC import gLogger
 
+from multiprocessing.pool import ThreadPool
+from multiprocessing import Manager
+from random import shuffle
+
+
+class dummyContextManager:
+    def __enter__(self):
+        return None
+    def __exit__(self, *args):
+        pass
+
 
 def doLs( directory, fc ):
   dirs = []
@@ -34,7 +45,7 @@ def doLs( directory, fc ):
   return (dirs, files)
 
 
-def getDirList( directory, depth, fc, exclude ):
+def getDirList( directory, depth, fc, exclude=None ):
   resd = []
   resf = []
 
@@ -44,7 +55,7 @@ def getDirList( directory, depth, fc, exclude ):
     (dirs, files) = doLs( directory, fc )
     resf = resf + files
     for f in dirs:
-      if f not in exclude:
+      if exclude is None or f not in exclude:
         (tdirs, tfiles) = getDirList( f, depth-1, fc, exclude )
         if tdirs:
           resd = resd + tdirs
@@ -57,6 +68,49 @@ def getDirList( directory, depth, fc, exclude ):
   return ( resd, resf )
 
 
+def findFiles( directory, fc, se, lock ):
+  print("find in {0}".format(directory), file=sys.stderr)
+  res = fc.getMetadataFields()
+  if not res['OK']:
+    gLogger.error( 'Can not access File Catalog:', res['Message'] )
+    DIRAC.exit( -1 )
+  typeDict = res['Value']['FileMetaFields']
+  typeDict.update( res['Value']['DirectoryMetaFields'] )
+  # Special meta tags
+  typeDict.update( FILE_STANDARD_METAKEYS )
+
+  mq = MetaQuery( typeDict = typeDict )
+  res = mq.setMetaQuery( [ 'Path=%s' % directory, 'SE=%s' % se ] )
+  if not res['OK']:
+    gLogger.error( "Illegal metaQuery:", res['Message'] )
+    DIRAC.exit( -1 )
+  metaDict = res['Value']
+  path = metaDict.pop( 'Path', directory )
+
+  res = fc.findFilesByMetadata( metaDict, path )
+  if not res['OK']:
+    gLogger.error( 'Can not find files from directory {0}: {1}'.format(directory, res['Message']) )
+    fres = None
+  else:
+    with lock:
+      for f in res['Value']:
+        print(f)
+    fres = 1
+  return fres
+
+def processDirectories( directories, fc, se, lock ):
+    failed = []
+    files = []
+    for dr in directories:
+        res = findFiles(dr, fc, se, lock)
+        if not res:
+          failed.append(dr)
+    return failed
+
+def getFiles( directories, se, lock ):
+    my_fc = FileCatalog()
+    failed = processDirectories(directories, fc, se, lock)
+    return failed
 
 
 if __name__ == "__main__":
@@ -65,11 +119,15 @@ if __name__ == "__main__":
   Script.registerSwitch( '', 'Path=', '    path in FileCatalog to be searched' )
   Script.registerSwitch( '', 'depth=', '    depth to be used during directory tree split. 3 by default' )
   Script.registerSwitch( '', 'threads=', '    Number of threads to use for file dumping' )
-#  Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
-#                                       'Usage:',
-#                                       '  %s [options] --Path=<PATH> --SE=<SE>' % Script.scriptName,
-#                                       ] )
-#                          )
+  Script.registerSwitch( '', 'SE=', '    Dump files only from this storage elements' )
+  #Script.setUsageMessage( '\n'.join(
+  #      [
+  #        __doc__.split( '\n' )[1],
+  #        'Usage:',
+  #        '  %s [options] --Path=<PATH> --SE=<SE>' % Script.scriptName,
+  #      ]
+  #    )
+  #  )
 
   Script.parseCommandLine( ignoreErrors = True )
 
@@ -81,9 +139,10 @@ if __name__ == "__main__":
   fc = FileCatalog()
 
   directory = '/lhcb'
-  exclude = [ '/lhcb/user', '/lhcb/debug' ]
   depth = 2
   rfiles = []
+  se = 'RAL-RDST'
+  nthreads = 3
 
   for opt, val in Script.getUnprocessedSwitches():
     if opt == 'Path':
@@ -92,6 +151,10 @@ if __name__ == "__main__":
       exclude = val.split(',')
     elif opt == 'depth':
       depth = int(val)
+    elif opt == 'SE':
+      se = val
+    elif opt == 'threads':
+      nthreads = int(val)
 
   res = fc.isDirectory(directory)
   if not res['OK']:
@@ -102,11 +165,23 @@ if __name__ == "__main__":
     gLogger.error("Path must be a directory, but %s is not" % directory)
     DIRAC.exit( -1 )
 
-  ( dirs, files ) = getDirList(directory, depth, fc, exclude)
-  print("DIRS:")
-  for d in dirs:
-    print(d)
+  ( dirs, files ) = getDirList(directory, depth, fc)
+  shuffle(dirs)
+  if nthreads > 1:
+    pool = ThreadPool(nthreads)
+    lst_size = len(dirs) // 3 + 1
+    with Manager() as manager:
+      lock = manager.Lock()
+      res = pool.starmap(getFiles, ((x, se, lock) for x in breakListIntoChunks(dirs, lst_size)))
+  else:
+    dummy_lock = dummyContextManager()
+    res = [getFiles(dirs, se, dummy_lock)]
 
-  print("\n\n\nFILES:")
+  print("\n\n\nUnchecked files")
   for f in files:
     print(f)
+
+  print("\n\n\nFAILED:")
+  for lst in res:
+    for d in lst:
+      print(d)
