942650290a83cb5f3a163a7d82fa822ca96b1bd2
Alexander Rogovskiy <alexander.rogovskiy@stfc.ac.uk>
2023-03-10 15:55:51 +0000

    feat: do not use rados cli commands
    These commands take a lot of time to execute, so let's avoid them.
    Instead we can use rados python api. This should be more effective
    since (at least) there is no need to establish context every time
    operation is executed.

--- search_stub.py
+++ search_stub.py
@@ -12,17 +12,28 @@ from tempfile import mkstemp
 DEF_NTHREADS = 1
 DEF_NPROCS = 1
 DEF_TMPDIR = '/tmp'
-DEF_OBJECT_SIZE = 64*1024*1024
 
 
-def stat(pool, object_name, striper=False):
-    striper_opt = ['--striper'] if striper else []
-    result = run(['rados', '-p', pool] + striper_opt + ['stat', object_name], stdout=PIPE, stderr=PIPE)
-    if result.returncode == 0:
-        _, _, res = result.stdout.decode('utf-8').rpartition(' ')
-        res = int(res)
+def filename2object(filename, obj_num):
+    return '{0}.{1:0>16x}'.format(filename, obj_num)
+
+
+def check_file(ctx, file_name, obj_count, object_size=None):
+    res = True
+    try:
+        size = int(ctx.get_xattr(filename2object(file_name, 0), 'striper.size'))
+    except (rados.NoData, rados.ObjectNotFound):
+        res = False
     else:
-        res = None
+        try:
+            last_obj_size = ctx.stat(filename2object(file_name, obj_count-1))[0]
+        except rados.ObjectNotFound:
+            res = False
+        else:
+            if object_size is None:
+                object_size = int(ctx.get_xattr(filename2object(file_name, 0), 'striper.layout.object_size'))
+            if object_size * (obj_count-1) + last_obj_size != size:
+                res = False
     return res
 
 
@@ -36,54 +47,65 @@ def sort_file(filename, tmpdir, ncpus=1):
                 par_opts = ['--parallel', ncpus]
             else:
                 par_opts = []
-            out = run(['sort'] + par_opts + [filename], stdout=fd)
+            out = run(['sort'] + par_opts + [filename], stdout=fd, env={'LC_COLLATE': 'C'})
         if out.returncode != 0:
             print("Failed to sort file {0}:\n{1}\n{2}".format(filename, out.stdout, out.stderr), file=sys.stderr)
-            os.path.unlink(sorted_path)
+            os.unlink(sorted_path)
             sorted_path = None
     return sorted_path
 
 
-def process_results(async_results, object_size):
-    for fn, oc, a1, a2 in async_results:
-        a1.wait()
-        a2.wait()
-        if not a1.successful() or not a2.successful():
-            print(fn)
+def process_results(async_results):
+    for filename, ares in async_results:
+        ares.wait()
+        if not ares.successful():
+            print(filename)
         else:
-            sz1, sz2 = a1.get(), a2.get() 
-            if sz1 + (oc - 1)*object_size != sz2:
-                print(fn)
+            if not ares.get():
+                print(filename)
 
 
-def find_stub(dump, ceph_pool, object_size, nprocs=2):
+def find_stub(dump, ceph_pool, object_size=None, nprocs=1, conffile='/etc/ceph/ceph.conf'):
+    cluster = rados.Rados(conffile=conffile)
+    cluster.connect()
+    ctx = cluster.open_ioctx(ceph_pool)
+
     async_results = []
     last_obj = None 
-    thread_pool = ThreadPool(nprocs)
+    thread_pool = None
+    if nprocs > 1:
+        thread_pool = ThreadPool(nprocs)
     obj_count = 0
+    line = None
     with open(dump) as fd:
-        for line in fd:
-            line = line.rstrip()
+        while line != '':
+            line = fd.readline().rstrip()
             filename = line[:-17]
             last_filename = last_obj[:-17] if last_obj else filename
             if filename != last_filename:
-                async_results.append(
-                        (last_filename, obj_count, thread_pool.apply_async(stat, (ceph_pool, last_obj)), thread_pool.apply_async(stat, (ceph_pool, last_filename, True)))
-                    )
+                fargs = (ctx, last_filename, obj_count, object_size)
+                if thread_pool:
+                    async_results.append(  ( last_filename, thread_pool.apply_async(check_file, fargs) )  )
+                else:
+                    print("checking |{0}|".format(last_filename), file=sys.stderr)
+                    if not check_file(*fargs):
+                        print(last_filename)
                 obj_count = 1
             else:
                 obj_count += 1
 
-            if len(async_results) > 100: 
-                process_results(async_results, object_size)
+            if len(async_results) > 10: 
+                process_results(async_results)
                 async_results = []
 
             last_obj = line
 
-    async_results.append(
-            (last_filename, obj_count, thread_pool.apply_async(stat, (ceph_pool, last_obj)), thread_pool.apply_async(stat, (ceph_pool, last_filename, True)))
-        )
-    process_results(async_results, object_size)
+
+    #async_results.append(
+    #        (last_filename, thread_pool.apply_async(stat, (ceph_pool, last_obj)), thread_pool.apply_async(stat, (ceph_pool, last_filename, True)))
+    #    )
+    process_results(async_results)
+    ctx.close()
 
 
 def parse_args():
@@ -92,7 +114,11 @@ def parse_args():
     parser.add_argument('-n', '--nthreads', help="Number of threads to use. Default is {0}.".format(DEF_NTHREADS), default=DEF_NTHREADS, type=int)
     parser.add_argument('-N', '--Nprocs', help="Number of processes to use for sort. Default is {0}.".format(DEF_NPROCS), default=DEF_NPROCS, type=int)
     parser.add_argument('-c', '--cleanup', help="Remove temporary files after exit.", action='store_true')
-    parser.add_argument('-o', '--object_size', help="Object size. Default is {0}".format(DEF_OBJECT_SIZE), type=int, default=DEF_OBJECT_SIZE)
+    parser.add_argument('-o', '--object_size', help="Object size. If omitted, it will be requested from each object." \
+            + "Such an approach has lower performance, but allow one to handle files with different object sizes",
+            type=int,
+            default=None
+        )
     gr = parser.add_mutually_exclusive_group()
     gr.add_argument('-s', '--sorted', help="Indicates that the file with object names is already sorted.", action='store_true')
     gr.add_argument('-t', '--tmpdir', help="Temporary directory to store sorted object dump. Default is {0}".format(DEF_TMPDIR), default=DEF_TMPDIR)
@@ -112,6 +138,6 @@ if __name__ == '__main__':
 
     if args.cleanup:
         if not args.sorted:
-            os.path.unlink(dump)
+            os.unlink(dump)
         else:
             print("Will not delete file {0} that was not created by me".format(dump), file=sys.stderr)
